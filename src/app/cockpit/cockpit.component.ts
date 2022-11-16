610d2c43fa88f07abf7d7802e2a2076f1abdca89
hammanater <hammanbraden24@g-apps.monett.k12.mo.us>
2022-11-16 14:17:24 -0600

    Doing some bug fixes
    went through and fixed a bug I had

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -28,7 +28,7 @@ export class CockpitComponent implements OnInit {
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit ({
-      serverName: this.nameInput.value,
+      serverName: nameInput.value,
       serverContent: this.newServerContent
     })
   }
