0cad4ce17b20f6a368695c60e85ee533fbd940ab
hammanater <hammanbraden24@g-apps.monett.k12.mo.us>
2022-11-30 13:09:49 -0600

    Seeing Lifecycle Hooks in Action
    finished lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,7 @@ onBlueprintAdded(blueprintData: {serverName: string, serverContent:string})
 onChangeFirst() {
   this.serverElements[0].name = 'Changed!';
 }
+onDestroyFirst() {
+  this.serverElements.splice(0, 1);
+}
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -3,25 +3,55 @@ OnInit,
 Input,
 ViewEncapsulation,
 OnChanges,
-SimpleChanges
-} from '@angular/core';
+SimpleChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy,
 
+} from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements
+OnInit,
+OnChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+OnDestroy,
+AfterViewChecked {
   @Input('srvElement') element: {type: string, name: string, content: string};
-  @Input() name: string;
+  @Input('name') name: string;
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges) {
-
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  ngDoCheck() {
+  }
+
+  ngAfterContentInit() {
+  }
+
+  ngAfterContentChecked() {
+  }
+
+  ngAfterViewInit() {
+  }
+
+  ngAfterViewChecked() {
+  }
+
+  ngOnDestroy() {
+  }
 }
